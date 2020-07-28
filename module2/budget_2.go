bb8f71256f26aa7575401244112a252423790d8e
penepenp <penepenp@gmail.com>
2020-07-27 22:32:32 -0400

    Completed module

--- module2/budget_2.go
+++ module2/budget_2.go
@@ -74,6 +74,9 @@ func (b *Budget) RemoveItem(description string) {
 // CreateBudget creates a new budget with a specified max
 func CreateBudget(month time.Month, max float32) (*Budget, error) {
 	var newBudget *Budget
+	if _, hasEntry := report[month]; hasEntry {
+		return nil, errDuplicateEntry
+	}
 	newBudget = &Budget{Max: max}
 	report[month] = newBudget
 	return newBudget, nil
